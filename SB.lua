2079045d9f780baef478352334d9eedd78ea5ad3
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-03-14 03:11:25 +0500

    Update SB.lua

--- SB.lua
+++ SB.lua
@@ -1,35 +1,10 @@
-while not game:IsLoaded() do
-    wait()
-end
 local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true))()
-local main = libary:CreateWindow("RNG Legends [IN-DEV]")
+local main = libary:CreateWindow("Planet Clicker")
 
-local mod = main:Button("RollEvent", function()
-_G.RollEvent_enabled = not _G.RollEvent_enabled
-
-while _G.RollEvent_enabled do
-game:GetService("ReplicatedStorage").Events.RollEvent:FireServer()
-
-wait(1)
-end
-end)
-
-local mod = main:Button("SendRebirthRequest", function()
-_G.SendRebirthRequest_enabled = not _G.SendRebirthRequest_enabled
-
-while _G.SendRebirthRequest_enabled do
-game:GetService("ReplicatedStorage").Events.SendRebirthRequest:FireServer()
-
-wait(1)
-end
-end)
-
-local mod = main:Button("SendAscensionRequest", function()
-_G.SendAscensionRequest_enabled = not _G.SendAscensionRequest_enabled
-
-while _G.SendAscensionRequest_enabled do
-game:GetService("ReplicatedStorage").Events.SendAscensionRequest:FireServer()
-
-wait(1)
+local mod = main:Button("ClickedButton", function()
+_G.ClickedButton_enabled = not _G.ClickedButton_enabled
+while _G.ClickedButton_enabled do
+game:GetService("ReplicatedStorage").ClickedButton:FireServer()
+wait(0.1)
 end
 end)
