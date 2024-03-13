a5dcf9576fe4f1900775c8b7b2fec7383df5b2a2
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-03-14 03:41:39 +0500

    Update SB.lua

--- SB.lua
+++ SB.lua
@@ -2,9 +2,13 @@ local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true
 local main = libary:CreateWindow("Planet Clicker")
 
 local mod = main:Button("ClickedButton", function()
-_G.ClickedButton_enabled = not _G.ClickedButton_enabled
-while _G.ClickedButton_enabled do
-game:GetService("ReplicatedStorage").ClickedButton:FireServer()
-wait(0.1)
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ToraScript/Script/main/CursedSea'))()
+end)
+
+local mod = main:Button("Effects:Destroy", function()
+game:GetService("Workspace").Effects:Destroy()
+end)
+
+local mod = main:Button("Anchored", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
 end)
