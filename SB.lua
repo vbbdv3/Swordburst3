831246732ecdb8b90b9f4cb32dde97966e8552d0
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-03-14 21:06:27 +0500

    Update SB.lua

--- SB.lua
+++ SB.lua
@@ -130,6 +130,8 @@ local args = {
 
 game:GetService("ReplicatedStorage").remotes.combat:FireServer(unpack(args))
 wait(0.1)
+game:GetService("ReplicatedStorage").remotes.dash:FireServer()
+wait(0.1)
     end
   end)
 
