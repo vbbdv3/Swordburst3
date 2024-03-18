b0c7da28553979cba0ce4d8c3ff999fd9ed39c15
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-03-18 17:18:52 +0500

    Update SB.lua

--- SB.lua
+++ SB.lua
@@ -13,6 +13,44 @@ wait(0.1)
       end
 end)
 
+local mod = main:Button("Cursed Chest", function()
+_G.Chestbuyc_enabled = not _G.Chestbuyc_enabled
+while _G.Chestbuyc_enabled do
+local args = {
+    [1] = "equipItem",
+    [2] = {
+        ["itemType"] = "chest",
+        ["amount"] = 14,
+        ["description"] = "Holds a mysterious item.",
+        ["equippable"] = "Open",
+        ["rarity"] = "Uncommon",
+        ["name"] = "Cursed Chest"
+    }
+}
+game:GetService("ReplicatedStorage").remotes.inventory:FireServer(unpack(args))
+wait(0.1)
+      end
+end)
+
+local mod = main:Button("Malevolent Chest", function()
+_G.Chestbuym_enabled = not _G.Chestbuym_enabled
+while _G.Chestbuym_enabled do
+local args = {
+    [1] = "equipItem",
+    [2] = {
+        ["itemType"] = "chest",
+        ["amount"] = 7,
+        ["description"] = "Holds a mysterious item.",
+        ["equippable"] = "Open",
+        ["rarity"] = "Rare",
+        ["name"] = "Malevolent Chest"
+    }
+}
+game:GetService("ReplicatedStorage").remotes.inventory:FireServer(unpack(args))
+wait(0.1)
+      end
+end)
+
 local mod = main:Button("TANK", function()
 wait(0.1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1581.916748046875, 100.074066162109375, 934.8900146484375)
