cb68894c27c2a4bc3a48ee2a7f776bf3ac954ed8
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-03-21 23:49:59 +0500

    Update SB.lua

--- SB.lua
+++ SB.lua
@@ -73,7 +73,7 @@ end)
 local mod = main:Button("TANK", function()
 _G.ADPt_enabled = not _G.ADPt_enabled
 while _G.ADPt_enabled do
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1581.916748046875, 100.074066162109375, 934.8900146484375)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1581.916748046875, 5.074066162109375, 934.8900146484375)
 wait(0.1)
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
 end
